e6c700346b479e2112526fb9fc598ed28371424e
Shaheem PP <shanofficial2000@gmail.com>
2024-05-26 17:16:39 -0400

    Initial Commit

---
+++ frontend/Horizon Card/Horizon Card/Horizon_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Horizon_CardApp.swift
+//  Horizon Card
+//
+//  Created by Shaheem PP on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Horizon_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
